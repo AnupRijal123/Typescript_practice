05c31df1ca316b9d779a725cb52d50576f048068
Anup <anuprijal321@gmail.com>
2024-06-20 17:17:03 +0545

    made edit todo functionality

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -3,6 +3,7 @@ import { MdDelete } from "react-icons/md";
 import { FaCheck } from "react-icons/fa";
 import { Todo } from '../model';
 import './SingleTodo.scss';
+import { useState } from 'react';
 
 type Props = {
     item: Todo,
@@ -12,6 +13,8 @@ type Props = {
 
 const SingleTodo = ({ item, todos, setTodos }: Props) => {
     console.log(item);
+    const [isEdit, setIsEdit] = useState<boolean>(false);
+    const [editTodo, setEditTodo] = useState<string>(item.todo);
     const handleCheck = (id: number) => {
         setTodos(todos.map((todo) => todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo));
     }
@@ -21,15 +24,34 @@ const SingleTodo = ({ item, todos, setTodos }: Props) => {
         setTodos(todos.filter((item) => item.id !== id));
     }
 
+    const handleEdit = (event: React.SyntheticEvent<EventTarget>) => {
+        event.preventDefault();
+        console.log(editTodo);
+        const newArray = todos.map((t) => t.id === item.id ? { ...t, todo: editTodo } : t);
+        setTodos(newArray);
+        setIsEdit(false);
+
+    }
+
     return (
-        <form className='single-todo-container'>
-            {item.isCompleted ? (
-                <s>{item.todo}</s>
-            ) : (
-                <p>{item.todo}</p>
+        <form className='single-todo-container' onSubmit={handleEdit}>
+            {isEdit === false && (
+                item.isCompleted ? (
+                    <s>{item.todo}</s>
+                ) : (
+                    <p>{item.todo}</p>
+                )
             )}
+
+            {isEdit &&
+
+                <input type="text" value={editTodo}
+                    onChange={(event) => setEditTodo(event.target.value)}
+                />
+            }
             <div className="icons-div">
-                <FaEdit className="icons" />
+                <FaEdit className="icons"
+                    onClick={() => setIsEdit(!isEdit)} />
                 <MdDelete className="icons"
                     style={{ color: 'red' }}
                     onClick={() => handleDelete(item.id)}
